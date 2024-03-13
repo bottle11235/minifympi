22055b3c2d6db6ed0329b1d794a5893d21443681
bottle11235 <bottle11235@gmail.com>
2024-03-13 19:16:25 +0800

    reset to old version

--- tests/test_dev__mpirun.py
+++ tests/test_dev__mpirun.py
@@ -7,33 +7,33 @@ from minifympi.dev import MinifyMPI
 mmp = MinifyMPI(4, 10)
 mmp.start_comm()
 
-# mmp.bcast['a'] = 1
-# a = mmp.gather['a']
-# mmp.log('a', a)
+mmp.bcast['a'] = 1
+a = mmp.gather['a']
+mmp.log('a', a)
 
 
-# data = np.arange(6).reshape(2, 3)
-# mmp.Bcast['b'] = data
-# b = mmp.Gather['b']
-# mmp.log('b', b)
+data = np.arange(6).reshape(2, 3)
+mmp.Bcast['b'] = data
+b = mmp.Gather['b']
+mmp.log('b', b)
 
 # 测试scatter
-# mmp.scatter['a'] = list(range(mmp.n_procs))
-# a = mmp.gather['a']
-# mmp.log('a', a)
+mmp.scatter['a'] = list(range(mmp.n_procs))
+a = mmp.gather['a']
+mmp.log('a', a)
 
-# # 测试Scatter
-# data = np.arange(mmp.n_procs*3).reshape(mmp.n_procs, -1)
-# mmp.Scatter['a'] = data
-# a = mmp.Gather['a']
-# mmp.log('a', a)
+# 测试Scatter
+data = np.arange(mmp.n_procs*3).reshape(mmp.n_procs, -1)
+mmp.Scatter['a'] = data
+a = mmp.Gather['a']
+mmp.log('a', a)
 
 
-# code = 'mmp.log("exec", mmp.gs["b"])'
-# mmp.exec(code)
+code = 'mmp.log("exec", mmp.gs["b"])'
+mmp.exec(code)
 
-# # mmp.log('tasks_count', mmp.tasks_count)
-# mmp.log("mmp.gs['a']", mmp.gs['a'])
+# mmp.log('tasks_count', mmp.tasks_count)
+mmp.log("mmp.gs['a']", mmp.gs['a'])
 
 
 # data = np.random.randint(0, 100, (15, 3))
